24c3feb3b2f12d04a733e4225ab6ed27a82b7308
pedro <phls@cin.ufpe.br>
2020-12-10 22:05:11 -0300

    staging

--- BeeLog/app/src/main/java/com/pedroso/beelog/GetLocation.kt
+++ BeeLog/app/src/main/java/com/pedroso/beelog/GetLocation.kt
@@ -9,23 +9,23 @@ import androidx.core.app.ActivityCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import com.pedroso.beelog.database.room.BeeLogRoomDatabase
 import com.pedroso.beelog.database.room.BeeLogRoomDatabase_Impl
 import com.pedroso.beelog.viewmodel.LocationViewModel
 import com.pedroso.beelog.viewmodel.LocationViewModelFactory
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
 class GetLocation(appContext: Context, workerParameters: WorkerParameters) : Worker(appContext, workerParameters) {
 
-//    private val Loca
+    var fusedLocationClient: FusedLocationProviderClient? = null
 
     private val repository = (applicationContext as LocationsApplication).repository
 
-//    {
-//        LocationViewModelFactory((application as LocationsApplication).repository)
-//    }
 
     override fun doWork(): Result {
         getAndSaveLocation()
@@ -33,15 +33,26 @@ class GetLocation(appContext: Context, workerParameters: WorkerParameters) : Wor
     }
 
     private fun getAndSaveLocation() {
-        lateinit var fusedLocationClient: FusedLocationProviderClient
 
         if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient((applicationContext))
-            var local: String? = null
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if(fusedLocationClient == null) {
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+            }
+            val apply = LocationRequest.create().apply {
+                fastestInterval=5000
+                priority=LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            val builder : LocationSettingsRequest.Builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(apply)
+            fusedLocationClient!!.lastLocation.addOnSuccessListener { location: Location? ->
                 Log.d("LOCATION", "aeee ${location}")
                 GlobalScope.launch {
-                    repository.insert(com.pedroso.beelog.database.data.Location(latitude = location!!.latitude, longitude =  location!!.longitude))
+                    repository.insert(
+                        com.pedroso.beelog.database.data.Location(
+                            latitude = location!!.latitude,
+                            longitude = location!!.longitude
+                        )
+                    )
                 }
             }
         }
